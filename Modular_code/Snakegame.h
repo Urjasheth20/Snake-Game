c95e270c5156fac7ab6e4664b3b964c1cab299af
Urjasheth20 <urja.sheth2005@gmail.com>
2025-02-12 02:57:59 +0530

    Update Snakegame.h

--- Modular_code/Snakegame.h
+++ Modular_code/Snakegame.h
@@ -32,43 +32,6 @@ private:
         SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
     }
    
-
-    void SpawnFood() {
-        bool valid;
-        do {
-            valid = true;
-            foodX = (rand() % (WIDTH - 4)) + 2;
-            foodY = (rand() % (HEIGHT - 4)) + 2;
-
-            if (foodX == x && foodY == y) valid = false;
-            for (int i = 0; i < nTail; i++) {
-                if (foodX == tailX[i] && foodY == tailY[i]) {
-                    valid = false;
-                    break;
-                }
-            }
-        } while (!valid);
-    }
-
-    void SpawnObstacles() {
-        
-        bool valid;
-        
-        for (int i = 0; i < nObstacles; i++) {
-            do {
-                valid = true;
-                obstaclesX[i] = (rand() % (WIDTH - 4)) + 2;
-                obstaclesY[i] = (rand() % (HEIGHT - 4)) + 2;
-                
-                if (obstaclesX[i] == x && obstaclesY[i] == y) valid = false;
-                if (obstaclesX[i] == foodX && obstaclesY[i] == foodY) valid = false;
-                for (int j = 0; j < nTail; j++) {
-                    if (obstaclesX[i] == tailX[j] && obstaclesY[i] == tailY[j]) valid = false;
-                }
-            } while (!valid);
-        }
-    }
-
 public:
     SnakeGame() : gameOver(false), direction(RIGHT), x(WIDTH / 2), y(HEIGHT / 2), score(0), nTail(2), highScore(0), nObstacles(0) {
     	nObstacles=5;
